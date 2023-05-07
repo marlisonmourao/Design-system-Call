132e5b3dade8ffc3a3810c5847e75a6e5de3a481
marlisonmourao <bentesmourao@gmail.com>
2023-05-07 04:08:57 -0400

    fix: add to storybook on production

--- packages/docs/.storybook/main.js
+++ packages/docs/.storybook/main.js
@@ -14,5 +14,12 @@ const config = {
   docs: {
     autodocs: "tag",
   },
+  viteFinal: (config, { configType }) => {
+    if(configType === 'PRODUCTION') {
+      config.base = '/Design-system-Call/'
+    }
+
+    return config
+  }
 };
 export default config;
